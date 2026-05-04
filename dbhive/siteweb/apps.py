d3eb79074158dac1e6817ead565ae202d62ab6f1
dalida14 <128805604+dalida14@users.noreply.github.com>
2026-05-04 22:33:06 +0000

    Update apps.py

--- dbhive/siteweb/apps.py
+++ dbhive/siteweb/apps.py
@@ -4,3 +4,17 @@ from django.apps import AppConfig
 class SitewebConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'siteweb'
+
+    def ready(self):
+        import os
+        from django.contrib.auth.models import User
+
+        username = "admin"
+        password = "admin12345"
+
+        if not User.objects.filter(username=username).exists():
+            User.objects.create_superuser(
+                username=username,
+                email="admin@gmail.com",
+                password=password
+            )
