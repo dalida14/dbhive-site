a2fd4c3966113c89c93341105536878a654b805a
dalida14 <128805604+dalida14@users.noreply.github.com>
2026-05-04 16:53:51 +0000

    Update settings.py

--- dbhive/dbhive/settings.py
+++ dbhive/dbhive/settings.py
@@ -133,3 +133,4 @@ if DEBUG:
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
