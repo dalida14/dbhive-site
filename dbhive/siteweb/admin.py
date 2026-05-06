09d2fbdece2d1712813b9a0a2abc3e9a9d108f36
dalida14 <128805604+dalida14@users.noreply.github.com>
2026-05-06 00:14:25 +0000

    Update admin.py

--- dbhive/siteweb/admin.py
+++ dbhive/siteweb/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import Article
 
-admin.site.register(Article)
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('titre', 'publie', 'date_publication')
+    list_filter = ('publie', 'categorie')
+    prepopulated_fields = {"slug": ("titre",)}
