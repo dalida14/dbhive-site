286e0d4779c024801daa3117d3c11887d4d05dea
Dalida Gouba <goubadalida@gmail.com>
2026-05-06 03:31:02 +0000

    clean database reset

--- dbhive/siteweb/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Article
-
-@admin.register(Article)
-class ArticleAdmin(admin.ModelAdmin):
-    list_display = ('titre', 'publie', 'date_publication')
-    list_filter = ('publie', 'categorie')
-    prepopulated_fields = {"slug": ("titre",)}
